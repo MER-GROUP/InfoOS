ed4d2dc47b3e01601b658faf9d0267b47abe8d15
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-08-16 12:10:01 +0300

    design-13

--- bind/Translate.py
+++ bind/Translate.py
@@ -5,35 +5,35 @@ class Translate:
     # словарь руского и английского языка
     translate = {
         'about': [
-            '[b][color=#C0C0C0]РАЗРАБОТЧИК И АВТОР:[/color][/b]\n' +
+            '[b][color=#2F4F4F]РАЗРАБОТЧИК И АВТОР:[/color][/b]\n' +
                     '[color=#808080]Максим Романенко (Red Alert)[/color]\n' +
-                '[b][color=#C0C0C0]ПРОГРАММНОЕ ОБЕСПЕЧЕНИЕ:[/color][/b]\n' +
+                '[b][color=#2F4F4F]ПРОГРАММНОЕ ОБЕСПЕЧЕНИЕ:[/color][/b]\n' +
                     '[color=#808080]свободное и распространяется\n' +
                     'по лицензии MIT[/color]\n' +
-                '[b][color=#C0C0C0]ИНСТРУМЕНТЫ РАЗРАБОТКИ:[/color][/b]\n' +
+                '[b][color=#2F4F4F]ИНСТРУМЕНТЫ РАЗРАБОТКИ:[/color][/b]\n' +
                     '[color=#808080]python (python.org), kivy (kivy.org),\n' +
                     'json (json.org), debian (debian.org),\n' +
                     'vscode (code.visualstudio.com),\n' +
                     'git (git-scm.com)[/color]\n' +
-                '[b][color=#C0C0C0]КОД ПРОГРАММЫ:[/color][/b]\n' +
-                    '[color=#808080]github.com/mer-group/kivycalc[/color]\n' +
-                '[b][color=#C0C0C0]КОНТАКТЫ:[/color][/b]\n' +
+                '[b][color=#2F4F4F]КОД ПРОГРАММЫ:[/color][/b]\n' +
+                    '[color=#808080]github.com/mer-group/infoos[/color]\n' +
+                '[b][color=#2F4F4F]КОНТАКТЫ:[/color][/b]\n' +
                     '[color=#808080]i@mer-group.ru\n' +
                     'github.com/mer-group[/color]',
 
-            '[b][color=#C0C0C0]DEVELOPER AND AUTHOR:[/color][/b]\n' +
+            '[b][color=#2F4F4F]DEVELOPER AND AUTHOR:[/color][/b]\n' +
                     '[color=#808080]Maxim Ramanenka (Red Alert)[/color]\n' +
-                '[b][color=#C0C0C0]SOFTWARE:[/color][/b]\n' +
+                '[b][color=#2F4F4F]SOFTWARE:[/color][/b]\n' +
                     '[color=#808080]free and distributed\n' +
                     'under the MIT license[/color]\n' +
-                '[b][color=#C0C0C0]DEVELOPMENT TOOLS:[/color][/b]\n' +
+                '[b][color=#2F4F4F]DEVELOPMENT TOOLS:[/color][/b]\n' +
                     '[color=#808080]python (python.org), kivy (kivy.org),\n' +
                     'json (json.org), debian (debian.org),\n' +
                     'vscode (code.visualstudio.com),\n' +
                     'git (git-scm.com)[/color]\n' +
-                '[b][color=#C0C0C0]PROGRAM CODE:[/color][/b]\n' +
-                    '[color=#808080]github.com/mer-group/kivycalc[/color]\n' +
-                '[b][color=#C0C0C0]CONTACTS:[/color][/b]\n' +
+                '[b][color=#2F4F4F]PROGRAM CODE:[/color][/b]\n' +
+                    '[color=#808080]github.com/mer-group/infoos[/color]\n' +
+                '[b][color=#2F4F4F]CONTACTS:[/color][/b]\n' +
                     '[color=#808080]i@mer-group.ru\n' +
                     'github.com/mer-group[/color]'
         ],
