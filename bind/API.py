73dac5b178a887520a064e8ff401b491020f784e
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-08-17 10:24:45 +0300

    add package_name_show

--- bind/API.py
+++ bind/API.py
@@ -8,9 +8,22 @@ if 'android' == platform:
     # autoclass - импорт java классов
     # JavaException - работа с исключениями java классов
     from jnius import autoclass, JavaException
+    # ---------------------------------------------------------------------------
+    # информация о приложении (программе)
+    # (public abstract class Context extends Object)
+    # https://developer.android.com/reference/android/content/Context
     Context = autoclass('android.content.Context')
+    # ---------------------------------------------------------------------------
+    #
+    #
+    #
     PackageManager = autoclass('android.content.pm.PackageManager')
+    # ---------------------------------------------------------------------------
+    #
+    #
+    #
     ApplicationInfo = autoclass('android.content.pm.ApplicationInfo')
+    # ---------------------------------------------------------------------------
     # информация об ОС Android
     # (public static class Build.VERSION extends Object)
     # https://developer.android.com/reference/android/os/Build.VERSION
@@ -34,6 +47,24 @@ class API:
             try:
                 return str(
                     VERSION.SDK_INT
+                    )
+            except JavaException as e:
+                return str(e)
+            except BaseException as e:
+                return str(e)
+        else:
+            # return 'Данный метод не реализован ...'
+            return 'This method is not implemented ...'
+    # ---------------------------------------------------------------------------
+    # Android:
+    #   Return the name of this application's package.
+    #   Возвращает имя пакета этого приложения.
+    #   https://developer.android.com/reference/android/content/Context#getPackageName()
+    def package_name_show(self) -> str:
+        if 'android' == platform:
+            try:
+                return str(
+                    Context.getPackageName()
                     )
             except JavaException as e:
                 return str(e)
