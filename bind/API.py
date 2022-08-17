c98062ebed1398f6ba7af80892d558ef8d8d657d
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-08-17 09:38:11 +0300

    add API

---
+++ bind/API.py
@@ -0,0 +1,53 @@
+# *****************************************************************************************
+# platform - определение операционки
+from kivy.utils import platform
+# *****************************************************************************************
+# если ОС Android, то загрузить следующие модули
+if 'android' == platform:
+    # ---------------------------------------------------------------------------
+    # autoclass - импорт java классов
+    # JavaException - работа с исключениями java классов
+    from jnius import autoclass, JavaException
+    Context = autoclass('android.content.Context')
+    PackageManager = autoclass('android.content.pm.PackageManager')
+    ApplicationInfo = autoclass('android.content.pm.ApplicationInfo')
+    # информация об ОС Android
+    # (public static class Build.VERSION extends Object)
+    # https://developer.android.com/reference/android/os/Build.VERSION
+    VERSION = autoclass('android.os.Build.VERSION')
+    # ---------------------------------------------------------------------------
+    # plyer - работа с железом устройства
+    # vibrator - управление вибрацией устройства
+    from plyer import vibrator
+    # ---------------------------------------------------------------------------
+# *****************************************************************************************
+# API - манипуляции (действия) с API опепационных систем
+class API:
+    # ---------------------------------------------------------------------------
+    # Android:
+    #   The SDK version of the software currently running on this hardware device.
+    #   Версия SDK программного обеспечения, запущенного в настоящее время 
+    #    на этом аппаратном устройстве.
+    #   https://developer.android.com/reference/android/os/Build.VERSION#SDK_INT
+    def sdk_show(self) -> str:
+        if 'android' == platform:
+            try:
+                return str(
+                    VERSION.SDK_INT
+                    )
+            except JavaException as e:
+                return str(e)
+            except BaseException as e:
+                return str(e)
+        else:
+            # return 'Данный метод не реализован ...'
+            return 'This method is not implemented ...'
+    # ---------------------------------------------------------------------------
+    pass
+    # ---------------------------------------------------------------------------
+# *****************************************************************************************
+# тесты
+# если не модуль то выполнить программу
+if __name__ == '__main__':
+    pass
+# *****************************************************************************************
