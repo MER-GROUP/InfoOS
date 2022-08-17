4c31dcc0b6fb4793ebd79e483e442ef47f4f47e5
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-08-17 12:16:11 +0300

    add package_name_installer_show

--- bind/API.py
+++ bind/API.py
@@ -14,10 +14,15 @@ if 'android' == platform:
     # https://developer.android.com/reference/android/content/Context
     Context = autoclass('android.content.Context')
     # ---------------------------------------------------------------------------
-    #
-    #
-    #
+    # дополнительная информация о приложении (программе)
+    # (public abstract class PackageManager extends Object)
+    # https://developer.android.com/reference/android/content/pm/PackageManager
     PackageManager = autoclass('android.content.pm.PackageManager')
+    # ---------------------------------------------------------------------------
+    # информация о том, как было установлено приложение
+    # (public final class InstallSourceInfo extends Object implements Parcelable)
+    # https://developer.android.com/reference/android/content/pm/InstallSourceInfo
+    InstallSourceInfo = autoclass('android.content.pm.InstallSourceInfo')
     # ---------------------------------------------------------------------------
     #
     #
@@ -38,6 +43,7 @@ if 'android' == platform:
 class API:
     # ---------------------------------------------------------------------------
     # Android:
+    # SDK_INT:
     #   The SDK version of the software currently running on this hardware device.
     #   Версия SDK программного обеспечения, запущенного в настоящее время 
     #    на этом аппаратном устройстве.
@@ -57,6 +63,7 @@ class API:
             return 'This method is not implemented ...'
     # ---------------------------------------------------------------------------
     # Android:
+    # getPackageName():
     #   Return the name of this application's package.
     #   Возвращает имя пакета этого приложения.
     #   https://developer.android.com/reference/android/content/Context#getPackageName()
@@ -65,6 +72,66 @@ class API:
             try:
                 return str(
                     Context.getPackageName()
+                    )
+            except JavaException as e:
+                return str(e)
+            except BaseException as e:
+                return str(e)
+        else:
+            # return 'Данный метод не реализован ...'
+            return 'This method is not implemented ...'
+    # ---------------------------------------------------------------------------
+    # Android:
+    # getPackageManager():
+    #   Return PackageManager instance to find global package information.
+    #   Возвращает объект PackageManager, чтобы найти глобальную информацию о пакете.
+    #   https://developer.android.com/reference/android/content/Context#getPackageManager()
+    #
+    # getInstallerPackageName(String packageName):
+    #   Retrieve the package name of the application that installed a package. 
+    #   This identifies which market the package came from.
+    #   Показывает установщик данного приложения.
+    #   Это позволяет определить с какого рынка (магазина) установленно данное приложение.
+    #   https://developer.android.com/reference/android/content/pm/PackageManager#getInstallerPackageName(java.lang.String)
+    #   This method was deprecated in API level 30.
+    #   Этот метод устарел на уровне API 30.
+    #   Use - getInstallSourceInfo(String packageName) if API 30 and higher.
+    #   Используйте - getInstallSourceInfo(String packageName) если API 30 и выше.
+    #
+    # getInstallSourceInfo(String packageName):
+    #   Retrieves information about how a package was installed or updated.
+    #   Извлекает информацию о том, как был установлен или обновлен пакет.
+    #   https://developer.android.com/reference/android/content/pm/PackageManager#getInstallSourceInfo(java.lang.String)
+    #
+    # getInstallingPackageName():
+    #   The name of the package responsible for the installation 
+    #    (the installer of record), or null if not available.
+    #   Имя пакета, ответственного за установку (установщик пакета), 
+    #    или null, если он недоступен.
+    #   https://developer.android.com/reference/android/content/pm/InstallSourceInfo#getInstallingPackageName()
+    #
+    # getPackageName():
+    #   Return the name of this application's package.
+    #   Возвращает имя пакета этого приложения.
+    #   https://developer.android.com/reference/android/content/Context#getPackageName()
+    def package_name_installer_show(self) -> str:
+        if 'android' == platform:
+            try:
+                if (30 > int(self.sdk_show())):
+                    return str(
+                        Context.getPackageManager().getInstallerPackageName(
+                            str(
+                                Context.getPackageName()
+                            )
+                        )
+                    )
+                else:
+                    return str(
+                        Context.getPackageManager().getInstallSourceInfo(
+                            str(
+                                Context.getPackageName()
+                            )
+                        ).getInstallingPackageName()
                     )
             except JavaException as e:
                 return str(e)
